79c18b7401fe2686aa572f17436db06af3c407aa
m0m013 <mabb01@protonmail.com>
2021-06-02 21:41:47 +0200

    modif message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Bijour Msiou!'));
+app.get('/', (req, res) => res.send('Bijour Msiou digoulausse va, salete bandi di spacouti !'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
